56d215103559dcd683b917cc94b9266c8da7aad9
uthurburudiego <42613913+uthurburudiego@users.noreply.github.com>
2023-09-25 03:03:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from pygame.locals import*
-import json
+from Clases.Class_Mundo import Mundo
+from pygame import mixer
 
-
-from Class_Mundo import Mundo
-
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
+mixer.init()
+fuente = "Recursos\Fuentes\GAMERIA.ttf"
+font = pygame.font.Font(fuente, 40)
+TEXT_COLOR = (255, 255, 255)
 
 clock = pygame.time.Clock()
 fps = 50
@@ -17,11 +20,8 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Megaman")
 
 
-# Carga de imagenes:
 background_image = pygame.transform.scale(pygame.image.load(
     "Recursos/Img/Maps/Niveles/background.png"), (width, height))
-
-
 
 
 mundo = Mundo(screen)
@@ -31,8 +31,8 @@ run = True
 while run:
     clock.tick(fps)
     screen.blit(background_image, (0, 0))
-
-    run = mundo.draw()
+    
+    run = mundo.update()
     #dibujar_cuadricula(screen, 50)
     lista_eventos = pygame.event.get()
     for event in lista_eventos:
